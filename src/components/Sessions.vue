1c3246b38e331a4353337082bf5cfd9dd8350cc7
Vinicius Albuquerque <viniciusalbuquerque@github.com>
2018-08-14 19:17:42 -0300

    Issue #16 - Using Material design to improve visuals
    Removed bootstrap dependency and added material design.
    Sessions page use material design components to better show
    notes.
    
    It fixes #16

--- src/components/Sessions.vue
+++ src/components/Sessions.vue
@@ -1,35 +1,43 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <div id="container">
-      <div
-        v-for="(notes, key) in notesMap"
-        v-bind:key="key"
-        class="div-topic">
+  <div class="page-container">
+    <md-app>
+    <md-app-toolbar class="md-primary">
+      <span class="md-title">{{ title }}</span>
+      <md-button class="md-primary md-raised bt-export" @click=export_click >
+        Export
+      </md-button>
+    </md-app-toolbar>
+    <md-app-content>
+      <div id="container">
+        <div
+          v-for="(notes, key) in notesMap"
+          v-bind:key="key"
+          class="div-topic">
 
-        <p class="topic-title">
-          {{key}}
-        </p>
+          <span class="md-display-1 topic-title" >
+            {{key}}
+          </span>
+            <md-card
+              v-for="note in notes"
+              header=" "
+              v-bind:key="note.id"
+              border-variant="secondary"
+              header-border-variant="secondary"
+              class="note-card text-center" >
+              <md-card-content>
+                    {{note.description}}
+              </md-card-content>
+              <md-card-actions>
+                <md-button class="md-primary" disabled>{{note.user}}</md-button>
+              </md-card-actions>
 
-        <b-card
-          v-for="note in notes"
-          v-bind:key="note.id"
-          v-bind:header="note.user"
-          border-variant="secondary"
-          header-border-variant="secondary"
-          class="note-card text-center" >
-            <p class="card-text">
-                {{note.description}}
-            </p>
-        </b-card>
-    </div>
-  </div>
-  <div id="div-export">
-    <button @click=export_click>Export</button>
-  </div>
+            </md-card>
+        </div>
+      </div>
+    </md-app-content>
+    </md-app>
   </div>
 </template>
 
 <script src="./Sessions/sessions.js"/>
-
 <style scoped type="text/css" src="./Sessions/sessions.css"/>
