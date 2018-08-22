c8081ba69c61c8c66948244fea3881ac2fabc906
Vinicius Albuquerque <viniciusalbuquerque@github.com>
2018-08-22 17:19:58 -0300

    Issue #24 - Topic and Note are components now
    Making topic and note be their own components. Removing css from
    the sessions css file and putting in their respective components
    css file.
    
    It fixes #24.

---
+++ src/components/Note.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="note-card" >
+        <p class="p-description">
+            {{note.description}}
+        </p>
+        <md-card-actions md-alignment="space-between" class="p-user">
+            {{note.user}}
+        </md-card-actions>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'note',
+  props: ['note']
+}
+</script>
+
+<style scoped type="text/css" src="./Note/note.css">
---
+++ src/components/Topic.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="div-topic">
+          <span class="md-display-1 topic-title" >
+            {{topic}}
+          </span>
+          <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'topic',
+  props: ['topic']
+}
+</script>
+
+<style scoped type="text/css" src="./Topic/topic.css">
