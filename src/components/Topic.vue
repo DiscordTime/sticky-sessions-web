779934357f891ba1dcc5efe99fff9d6ca3696c65
Vinicius Albuquerque <viniciusalbuquerque@github.com>
2018-09-13 10:59:09 -0300

    Issue #32: Filtering by author
    By clicking on the author's name, the notes that will be shown
    are only the ones that were written by that author.
    
    Also removed the click of filtering by topic of the whole div and
    applied only on the topic name.
    
    It fixes #32.

--- src/components/Topic.vue
+++ src/components/Topic.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="subcontainer-topic">
-          <a class="md-display-1 topic-title">
+          <a
+            class="md-display-1 topic-title"
+            v-on:click="filter_by_topic">
             {{topic}}
           </a>
           <div v-bind:class="note_container_class">
