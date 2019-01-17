ad022dfca2de3cae805783553a09a9c537e86f15
Amaro Neto <amaro@motorola.com>
2019-01-17 16:47:39 -0300

    Updated index layout

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,22 +1,32 @@
 <template>
+
 <div id="page-container">
-    <div id="toolbar">
-      <md-app-toolbar class="md-primary">
-        <span class="md-title">{{ title }}</span>
-      </md-app-toolbar>
-    </div>
-    <div>
+    <md-app md-waterfall md-mode="fixed">
+    <md-app-toolbar class="md-primary">
+        <span class="md-title">Sticky Notes</span>
+        <div class="md-toolbar-section-end">
+            <md-button>Criar Sessão</md-button>
+        </div>
+    </md-app-toolbar>
+    <md-app-content>
       <md-list>
         <md-list-item
           md-secondary
           v-for="session in sessions"
           v-bind:key="session.id"
           v-bind:to="'/sessions/'+session.id">
-          {{ session.name }}
-          {{ session.time }}
+          <div class="md-list-item-text">
+              <span class="md-title list-title">{{ session.name }}</span>
+              <span class="md-subheading list-subtitle">{{ session.time }}</span>
+              <md-badge class="md-square" md-content="Aberto" />
+          </div>
+
         </md-list-item>
+
       </md-list>
-    </div>
+    </md-app-content>
+    </md-app>
+
 </div>
 </template>
 
