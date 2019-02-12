7506650e18aaa7abe81d45fdd526f4239413d232
Carlos Rodrigo Garcia <carlosrodrigocrcg@gmail.com>
2019-02-12 12:34:06 -0300

    Issue #55: Add setup script to CircleCI config yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ "$IMAGE_NAME" = "" ]; then
+if [ -z "$IMAGE_NAME" ]; then
   echo "IMAGE_NAME env variable is empty. Exiting."
   exit 1
 fi
 
-if [ "$GOOGLE_PROJECT_ID" = "" ]; then
+if [ -z "$GOOGLE_PROJECT_ID" ]; then
   echo "GOOGLE_PROJECT_ID env variable is empty. Exiting."
   exit 1
 fi
@@ -13,14 +13,14 @@ fi
 # Authenticate to Google Container Registry
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://gcr.io
 
-# Builds Docker image
-docker build -t ${IMAGE_NAME} .
-
 if [ "$CIRCLE_BRANCH" = "dev" ]; then
   echo "Deploying application to Staging environment"
 
   # Injects firebase DB file
-  echo $FIREBASE_DB_STAG > src/db/firebaseDB/keys/index.js
+  echo $FIREBASE_DB_STAG | base64 -di > src/db/firebaseDB/keys/index.js
+
+  # Builds Docker image
+  docker build -t ${IMAGE_NAME} .
 
   # Tags newly created image
   docker tag ${IMAGE_NAME} gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:stag
@@ -29,12 +29,15 @@ if [ "$CIRCLE_BRANCH" = "dev" ]; then
   docker push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:stag
   
   # Deploys application
-  gcloud --quiet compute ssh stick-sessions --zone us-east1-b -- 'cd /home/app && ./restart_client_stag.sh'
+  gcloud --quiet compute ssh stick-sessions --zone us-east1-b -- "cd /home/app && ./restart_service.sh gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:stag"
 elif [ "$CIRCLE_BRANCH" = "master" ]; then
   echo "Deploying application to Production environment"
 
   # Injects firebase DB file
-  echo $FIREBASE_DB > src/db/firebaseDB/keys/index.js
+  echo $FIREBASE_DB | base64 -di > src/db/firebaseDB/keys/index.js
+
+  # Builds Docker image
+  docker build -t ${IMAGE_NAME} .
 
   # Tags newly created image
   docker tag ${IMAGE_NAME} gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:${CIRCLE_SHA1}
@@ -45,5 +48,5 @@ elif [ "$CIRCLE_BRANCH" = "master" ]; then
   docker push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:latest
 
   # Deploys application
-  gcloud --quiet compute ssh stick-sessions --zone us-east1-b -- 'cd /home/app && ./restart_client.sh'
+  gcloud --quiet compute ssh stick-sessions --zone us-east1-b -- "cd /home/app && ./restart_service.sh gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:latest"
 fi
