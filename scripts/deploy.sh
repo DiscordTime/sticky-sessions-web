68ad40f14bbff7c706285d8be280292baf1bd333
Carlos Rodrigo Garcia <carlosrodrigocrcg@gmail.com>
2019-02-25 17:55:57 -0300

    Issue #55: Update CircleCI config to automatically deploy application
    The build config now creates a new Docker image, tags it with latest, pushes it to
    Google Container Registry and deploys to Google Compute Engine.

---
+++ scripts/deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+if [ -z "$IMAGE_NAME" ]; then
+  echo "IMAGE_NAME env variable is empty. Exiting."
+  exit 1
+fi
+
+if [ -z "$GOOGLE_PROJECT_ID" ]; then
+  echo "GOOGLE_PROJECT_ID env variable is empty. Exiting."
+  exit 1
+fi
+
+# Authenticate to Google Container Registry
+gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://gcr.io
+
+if [ "$CIRCLE_BRANCH" = "issue_55_cd" ]; then
+  echo "Deploying application to Staging environment"
+
+  # Injects firebase DB file
+  echo $FIREBASE_DB_STAG | base64 -di > src/db/firebaseDB/keys/index.js
+
+  # Builds Docker image
+  docker build -t ${IMAGE_NAME} .
+
+  # Tags newly created image
+  docker tag ${IMAGE_NAME} gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:stag
+  
+  # Pushes image to Container Registry
+  docker push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:stag
+  
+  # Deploys application
+  gcloud --quiet compute ssh stick-sessions --zone us-east1-b -- "cd /home/app && ./restart_service.sh gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:stag"
+elif [ "$CIRCLE_BRANCH" = "master" ]; then
+  echo "Deploying application to Production environment"
+
+  # Injects firebase DB file
+  echo $FIREBASE_DB | base64 -di > src/db/firebaseDB/keys/index.js
+
+  # Builds Docker image
+  docker build -t ${IMAGE_NAME} .
+
+  # Tags newly created image
+  docker tag ${IMAGE_NAME} gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:${CIRCLE_SHA1}
+  docker tag ${IMAGE_NAME} gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:latest
+
+  # Pushes image to Container Registry
+  docker push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:${CIRCLE_SHA1}
+  docker push gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:latest
+
+  # Deploys application
+  gcloud --quiet compute ssh stick-sessions --zone us-east1-b -- "cd /home/app && ./restart_service.sh gcr.io/${GOOGLE_PROJECT_ID}/${IMAGE_NAME}:latest"
+fi
