767cb2d6aa68ab108f7eb9ac58b13871a1788fc4
Carlos Rodrigo Garcia <carlosrodrigocrcg@gmail.com>
2020-09-29 20:36:12 -0300

    Set session title using info on session model
    Remove algorithm to set session title by topics length
    and use title set on session model

--- src/db/firebaseDB/mapper.js
+++ src/db/firebaseDB/mapper.js
@@ -20,13 +20,8 @@ module.exports.parserSnapshotToSessions = function (querySnapshot, callback) {
     const formatted = moment(moment.unix(session.timestamp.seconds)).format('DD-MM-YYYY HH:mm')
     session['id'] = doc.id
     session['time'] = formatted
+    session['name'] = session.title
 
-    // TODO: Remove when app define the actual name.
-    if (session.topics.length === 4) {
-      session['name'] = 'Gain & Pleasure'
-    } else {
-      session['name'] = 'Starfish'
-    }
     sessions.push(session)
   })
   callback(sessions)
